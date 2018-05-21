2ce4030788d40e5f0e32d416f76c06da7b75788d
caloloc2 <calolomino@gmail.com>
2018-05-21 15:25:08 -0500

    actualiza

--- control.c
+++ control.c
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <wiringPi.h>
+#include <time.h>
+//#include <wiringPi.h>
 
 enum {EXITO, FALLO};
 
@@ -12,66 +13,80 @@ int main(void){
 	pinMode(0, OUTPUT); // GPIO 17 
 	pinMode(2, OUTPUT); // GPIO 27 
 
-	FILE *ptr_fichero;
+	FILE *ptr_fichero, *tmp_fichero;
 	char nombre_fichero[] = "estados.rsp";
+	char fichero_tiempo[] = "hora.rsp";
 	int resultado = EXITO;
+	char buffer[3];
+	char tiempo_dato[10];
+	char output[128];
+	int todos = 0;	
 
 	while(1){
-		if ( (ptr_fichero = fopen(nombre_fichero, "r") ) == NULL){
+
+		time_t tiempo = time(0);
+	    struct tm *tlocal = localtime(&tiempo);    
+	    strftime(output,128,"%H:%M",tlocal);    
+
+		if ((ptr_fichero = fopen(nombre_fichero, "r")) == NULL){
 			printf("No se ha podido abrir o no se encuentra el fichero %s.\n", nombre_fichero);
 			resultado = FALLO;
-		}else{
-			char buffer[3];
+		}else{		
 			while(!feof(ptr_fichero)){
 				fgets(buffer, 3, ptr_fichero);
 			}
-
-			/*
-			switch (buffer[0]){  
-				case '1':
-					// apaga rele 1
-					reles[0] = 1;
-					break;  
-				case '2':  
-					// prende rele 1
-					reles[0] = 2;
-					break;  
-				case '3':  
-					// apaga rele 2
-					reles[1] = 3;
-					break;  
-				case '4':  
-					// prende rele 1
-					reles[1] = 4;
-					break;  
-				case '0':  
-					// no hace nada
-					reles[0] = 0;
-					reles[1] = 0;
-					break;  
-			}		*/
 
 			if (fclose(ptr_fichero)!=0){
 				printf("No se ha podido cerrar el fichero %s.\n", nombre_fichero);
 				resultado = FALLO;
 			}else{
-				// luego de cerrar el archivo, acciona los pines de la raspberry segun corresponda
-				if (buffer[0]=='2'){
-					printf("prende rele 1\n");
-					digitalWrite(0, HIGH);
-				}else if(buffer[0]=='1'){
-					printf("apaga rele 1\n");
-					digitalWrite(0, LOW);
-				}else if(buffer[0]=='4'){
-					printf("prende rele 2\n");
-					digitalWrite(2, HIGH);
-				}else if(buffer[0]=='3'){
-					printf("apaga rele 2\n");
-					digitalWrite(2, LOW);
-				}
+				
 			}
 		}	
-		delay(500);
+
+	    if (todos==0){
+		    if ((tmp_fichero = fopen(fichero_tiempo, "r")) == NULL){
+				printf("No se ha podido abrir o no se encuentra el fichero %s.\n", fichero_tiempo);
+				resultado = FALLO;
+			}else{		
+				while(!feof(tmp_fichero)){
+					fgets(tiempo_dato, 10, tmp_fichero);
+				}
+
+				if (fclose(tmp_fichero)!=0){
+					printf("No se ha podido cerrar el fichero %s.\n", fichero_tiempo);
+					resultado = FALLO;
+				}
+				
+				if (strcmp(tiempo_dato, output)==0){
+					todos = 1;
+				}
+			}		
+	    }
+		
+
+		// luego de cerrar el archivo, acciona los pines de la raspberry segun corresponda
+		if (todos==0){
+			if (buffer[0]=='2'){
+				//printf("prende rele 1\n");
+				digitalWrite(0, HIGH);
+			}else if(buffer[0]=='1'){
+				//printf("apaga rele 1\n");
+				digitalWrite(0, LOW);
+			}else if(buffer[0]=='4'){
+				//printf("prende rele 2\n");
+				digitalWrite(2, HIGH);
+			}else if(buffer[0]=='3'){
+				//printf("apaga rele 2\n");
+				digitalWrite(2, LOW);
+			}
+		}else{
+			//printf("prende rele 1-2\n");
+			digitalWrite(0, HIGH);
+			digitalWrite(2, HIGH);			
+		}
+		
+		delay(300);
 	}
 	
 	return resultado;
--- hora.php
+++ hora.php
@@ -1,10 +1,17 @@
 <?php 
+function Escribe($dato){
+	$fp = fopen("hora.rsp", "w");
+	fputs($fp, $dato);
+	fclose($fp);
+}
 
 if (isset($_POST['hora'])){	
 	$data['respuesta'] = "Los reles se activarán a las ".$_POST['hora'];
+	Escribe($_POST['hora']);
 	$respuesta[] = $data;
 }else{	
 	$data['respuesta'] = "Error al configurar hora.";
+	Escribe("");
 	$respuesta[] = $data;
 }
 
