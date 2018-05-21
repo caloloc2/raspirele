5d8a46dd4e51626372498b9aebdaa1f5ef83c33a
caloloc2 <calolomino@gmail.com>
2018-05-21 14:57:18 -0500

    actualiza

--- control.c
+++ control.c
@@ -28,6 +28,7 @@ int main(void){
 				fgets(buffer, 3, ptr_fichero);
 			}
 
+			/*
 			switch (buffer[0]){  
 				case '1':
 					// apaga rele 1
@@ -50,30 +51,29 @@ int main(void){
 					reles[0] = 0;
 					reles[1] = 0;
 					break;  
-			}		
+			}		*/
 
 			if (fclose(ptr_fichero)!=0){
 				printf("No se ha podido cerrar el fichero %s.\n", nombre_fichero);
 				resultado = FALLO;
 			}else{
 				// luego de cerrar el archivo, acciona los pines de la raspberry segun corresponda
-				if (reles[0]==2){
+				if (buffer[0]=='2'){
 					printf("prende rele 1\n");
 					digitalWrite(0, HIGH);
-				}else if(reles[0]==1){
+				}else if(buffer[0]=='1'){
 					printf("apaga rele 1\n");
 					digitalWrite(0, LOW);
-				}else if (reles[1]==4){
+				}else if(buffer[0]=='4'){
 					printf("prende rele 2\n");
 					digitalWrite(2, HIGH);
-				}else if(reles[1]==3){
+				}else if(buffer[0]=='3'){
 					printf("apaga rele 2\n");
 					digitalWrite(2, LOW);
-				}else{
-					// nada
 				}
 			}
 		}	
+		delay(500);
 	}
 	
 	return resultado;
